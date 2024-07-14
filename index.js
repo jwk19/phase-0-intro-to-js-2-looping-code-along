92fb6b9f56a60fdd5a23d3246e03a9e24b026b89
James <spesalink@gmail.com>
2024-07-14 12:38:32 +0300

    Practised the while loop with the countDown function and the for loop with the writeCards function.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// function for the countdown while loop
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  countDown(10); //calling the function to countdown from 10
+
+  // function for the for loop writeCards
+  function writeCards(names, event) {
+    let messages = []; // Create an empty array to hold the messages
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages; // Return the array of messages
+  }
+
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday")); //testing the writeCards function
